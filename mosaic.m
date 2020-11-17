118088bad6706d2262b7e1e77625f5da797c6deb
D. Ben Knoble <ben.knoble+github@gmail.com>
2020-11-17 14:32:55 -0500

    apparently it is good style to use commas

--- mosaic.m
+++ mosaic.m
@@ -3,12 +3,12 @@
 addpath('mat2tiles');
 
 J = im2double(imread('keyart-resized/twilightprincess.jpg'));
-[rows cols n] = size(J);
+[rows, cols, n] = size(J);
 block_size = 75;
 n_blocks = rows / 75 * cols / 75;
 
 chunks = mat2tiles(J, [block_size, block_size]);
-[row_chunks col_chunks] = size(chunks);
+[row_chunks, col_chunks] = size(chunks);
 blocks = zeros(row_chunks, col_chunks, 75, 75, 3);
 for i = 1:row_chunks
     for j = 1:col_chunks
