335745c88aad69918e290714142f6b823d3f2bc6
D. Ben Knoble <ben.knoble+github@gmail.com>
2020-11-19 17:27:05 -0500

    remove images that are not 75x75x3

---
+++ remove_bad_thumbs.m
@@ -0,0 +1,14 @@
+thumbnail_dir = 'thumbnails';
+thumbss = dir(fullfile(thumbnail_dir, '*_*'));
+for inner_dir_i = 1:numel(thumbss)
+    inner_dir = thumbss(inner_dir_i);
+    thumbs = read_images(fullfile(inner_dir.folder, inner_dir.name, '*_*'));
+    disp(inner_dir.name)
+    disp(size(thumbs))
+    for thumb = 1:numel(thumbs)
+        if numel(size(thumbs(thumb).I)) ~= numel([75 75 3])
+            disp(fullfile(thumbs(thumb).folder, thumbs(thumb).name))
+            % delete(fullfile(thumbs(thumb).folder, thumbs(thumb).name))
+        end
+    end
+end
