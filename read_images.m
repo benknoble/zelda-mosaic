b3e3360ed09994a928a7c0fa798ba5f4e47b17c5
D. Ben Knoble <ben.knoble+github@gmail.com>
2020-12-09 13:20:33 -0500

    fix imread usage to handle indexed images

--- read_images.m
+++ read_images.m
@@ -2,6 +2,13 @@ function images = read_images(glob)
     images = dir(glob);
     num_images = length(images);
     for k=1:num_images
-        images(k).I = im2double(imread(fullfile(images(k).folder, images(k).name)));
+        [I, map, ~] = imread(fullfile(images(k).folder, images(k).name));
+        % some images are "indexed" and need colormaps
+        % they will be grayscale (size(I) = [x y]) until converted to rgb
+        % at which point size(I) = [x y 3]
+        if numel(size(I)) ~= 3
+            I = ind2rgb(I, map);
+        end
+        images(k).I = im2double(I);
     end
 end
