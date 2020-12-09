6194cc5de4035d90107ee169fb2a209aec054a5f
D. Ben Knoble <ben.knoble+github@gmail.com>
2020-12-09 13:20:16 -0500

    make it count them
    can always change commented lines to print/delete

--- remove_bad_thumbs.m
+++ remove_bad_thumbs.m
@@ -1,14 +1,17 @@
 thumbnail_dir = 'thumbnails';
 thumbss = dir(fullfile(thumbnail_dir, '*_*'));
 for inner_dir_i = 1:numel(thumbss)
+    num = 0;
     inner_dir = thumbss(inner_dir_i);
     thumbs = read_images(fullfile(inner_dir.folder, inner_dir.name, '*_*'));
     disp(inner_dir.name)
     disp(size(thumbs))
     for thumb = 1:numel(thumbs)
         if numel(size(thumbs(thumb).I)) ~= numel([75 75 3])
-            disp(fullfile(thumbs(thumb).folder, thumbs(thumb).name))
+            num = num + 1;
+            % disp(fullfile(thumbs(thumb).folder, thumbs(thumb).name))
             % delete(fullfile(thumbs(thumb).folder, thumbs(thumb).name))
         end
     end
+    disp(num)
 end
